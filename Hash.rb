b4639918be71cc314f49e430be38ce3ae4ea3d8e
Elise Polentes <Elise@Elise-Polentess-MacBook-Pro.local>
2020-04-08 11:26:38 -0600

    Hash.rb

--- Hash.rb
+++ Hash.rb
@@ -24,3 +24,23 @@ def retrieve_values(hash1, hash2, key) #takes in two hashes and a key, returns t
     print array
   end
 
+  def cat_builder(name_str, color_str, age_num) #creates a hash with name, age and color as keys for the values taken into the method.
+    hash = {"name" => name_str, "color" => color_str, "age" =>age_num}
+    puts hash
+    
+  end
+
+  def ae_count(str) #takes in a string and returns a hash a key value pair for the number of "a" and "e" in the string.
+    count_a = 0
+    count_e = 0
+    str.each_char do |letter|  
+      if letter == "a"  
+        count_a +=1
+      elsif letter == "e"
+        count_e +=1
+      end
+    end  
+      return {"a" => count_a, "e" => count_e} 
+  end
+  
+  
